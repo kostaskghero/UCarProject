0dff000d79a64cdf5d9e6c25af4e352b68eefe91
kes <kes@kes-PC>
2015-06-20 11:01:10 +0900

    member.sql sharing.sql 정리

--- UCarProject/src/main/resources/member.sql
+++ UCarProject/src/main/resources/member.sql
@@ -1,3 +1,4 @@
+-- member
 drop table member;
 create table member(
 	member_id varchar2(50) primary key,
@@ -5,13 +6,29 @@ create table member(
 	member_name varchar2(50) not null,
 	member_email varchar2(50) not null,
 	member_phone varchar2(50) not null,
-	member_point number default 0,
-	member_level varchar2(50) default 1
+	member_point number default 0 not null,
+	member_level varchar2(50) default 1 not null,
+	member_register_date date not null
 )
 select * from member;
-insert into member(member_id, member_password, member_name, member_email, member_phone)
-values ('java', '1234', '한효주', 'skghero@naver.com', '01020636416');
-
+-- admin
+insert into member(member_id, member_password, member_name, member_email, member_phone, member_level, member_register_date)
+values('admin','1234','관리자','ucar@gamil.com','01000000000', 2, sysdate);
+-- member
+-- 카드1개등록
+insert into member(member_id, member_password, member_name, member_email, member_phone, member_register_date)
+values('java', '1234', '박서준', 'java@ucar.com', '01000000000', sysdate);
+-- 면허등록
+insert into member(member_id, member_password, member_name, member_email, member_phone, member_register_date)
+values('hee', '1234', '유경희', 'hee@giveticon.com', '01000000000', sysdate);
+-- 카드3개등록
+insert into member(member_id, member_password, member_name, member_email, member_phone, member_register_date)
+values('minions', '1234', '케빈', 'banana@mini.com', '01000000000', sysdate);
+-- 면허등록, 카드2개등록
+insert into member(member_id, member_password, member_name, member_email, member_phone, member_register_date)
+values('bobbob', '1234', '밥', 'bobbob@mini.com', '01000000000', sysdate);
+	
+-- 면허정보
 drop table driving_license;
 create table driving_license(
 	member_id varchar2(50),
@@ -25,9 +42,15 @@ create table driving_license(
 	constraint pk_member_id primary key(member_id)
 )
 select * from driving_license;
-insert into driving_license (license_no, member_id, license_type, license_renewal_term, license_issue_date, gender, license_birth)
-values ('1', 'java', '1종보통', to_date('2015/04/10','YYYY/MM/DD'),to_date('2010/02/22','YYYY/MM/DD'), '여', to_date('1988/11/11','YYYY/MM/DD'));
 
+insert into driving_license(license_no, member_id, license_type, license_renewal_term, license_issue_date, gender, license_birth)
+values('서울 11-010054-23', 'hee', '2종보통', to_date('2022-12-31','YYYY/MM/DD'), 
+	to_date('2010-12-24','YYYY/MM/DD'), '여', to_date('1990-11-03','YYYY/MM/DD'));
+insert into driving_license(license_no, member_id, license_type, license_renewal_term, license_issue_date, gender, license_birth)
+values('강원 13-032387-41', 'bobbob', '1종보통', to_date('2020-12-31','YYYY/MM/DD'), 
+	to_date('2016-10-24','YYYY/MM/DD'), '남', to_date('1990-05-30','YYYY/MM/DD'));
+		
+-- 결제카드
 drop table card
 create table card(
 	card_no varchar2(50) primary key,
@@ -38,11 +61,24 @@ create table card(
 	card_birth date not null,
 	constraint fk_card_id foreign key(member_id) references member	
 )
-select * from card
-insert into card(card_no, member_id, card_type, card_expiration_date, card_password, card_birth) 
-		values('11', 'java', '개인', to_date('2018/02','YYYY/MM'), '1234', to_date('1900/10/11','YYYY/MM/DD'));
+select * from card;
 
+insert into card(card_no, member_id, card_type, card_expiration_date, card_password, card_birth) 
+values('5021-2377-5289-3307', 'minions', '개인', to_date('2018-02','YYYY/MM'), '50', to_date('1993-10-23','YYYY/MM/DD'));
+insert into card(card_no, member_id, card_type, card_expiration_date, card_password, card_birth) 
+values('3922-4739-2639-2261', 'minions', '개인', to_date('2018-02','YYYY/MM'), '11', to_date('1993-10-23','YYYY/MM/DD'));
+insert into card(card_no, member_id, card_type, card_expiration_date, card_password, card_birth) 
+values('3724-3923-2422-5457', 'minions', '개인', to_date('2018-02','YYYY/MM'), '11', to_date('1993-10-23','YYYY/MM/DD'));
+insert into card(card_no, member_id, card_type, card_expiration_date, card_password, card_birth) 
+values('8745-2342-5465-4336', 'java', '개인', to_date('2018-02','YYYY/MM'), '11', to_date('1982-07-14','YYYY/MM/DD'));
+insert into card(card_no, member_id, card_type, card_expiration_date, card_password, card_birth) 
+values('4592-8743-2687-1277', 'bobbob', '개인', to_date('2020-02','YYYY/MM'), '11', to_date('1990-05-30','YYYY/MM/DD'));
+insert into card(card_no, member_id, card_type, card_expiration_date, card_password, card_birth) 
+values('2342-3794-2349-4568', 'bobbob', '개인', to_date('2018-02','YYYY/MM'), '11', to_date('1990-05-30','YYYY/MM/DD'));
+	
+-- 운전면허 지역정보
 create sequence license_location_seq nocache;
+drop sequence license_location_seq;
 drop table license_location;
 create table license_location(
 	license_location_no number primary key,
@@ -80,3 +116,27 @@ insert into LICENSE_LOCATION values(license_location_seq.nextval, '23');
 insert into LICENSE_LOCATION values(license_location_seq.nextval, '24');
 insert into LICENSE_LOCATION values(license_location_seq.nextval, '25');
 insert into LICENSE_LOCATION values(license_location_seq.nextval, '26');
+
+-- 포인트
+drop sequence point_history_seq;
+create sequence point_history_seq nocache;
+drop table point_history;
+create table point_history(
+	point_history_no number primary key,
+	member_id varchar2(50),
+	point_value number not null,
+	point_date date not null,
+	point_content varchar2(100) not null,
+	point_type varchar2(50) not null,
+	constraint fk_point_member_id foreign key (member_id) references member
+)
+select * from point_history;
+
+insert into point_history(point_history_no, member_id, point_value, point_date, point_content, point_type)
+values(point_history_seq.nextval, 'java', 10000, sysdate, '회원가입', '적립');
+insert into point_history(point_history_no, member_id, point_value, point_date, point_content, point_type)
+values(point_history_seq.nextval, 'hee', 10000, sysdate, '회원가입', '적립');
+insert into point_history(point_history_no, member_id, point_value, point_date, point_content, point_type)
+values(point_history_seq.nextval, 'minions', 10000, sysdate, '회원가입', '적립');
+insert into point_history(point_history_no, member_id, point_value, point_date, point_content, point_type)
+values(point_history_seq.nextval, 'bobbob', 10000, sysdate, '회원가입', '적립');
